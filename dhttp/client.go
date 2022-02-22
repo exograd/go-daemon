30111836d26a5379a8f2a8500abe2ee2aafa8cd3
Nicolas Martyanoff <nicolas.martyanoff@exograd.com>
2022-02-22 10:02:26 +0100

    split generic http client and api client

--- dhttp/client.go
+++ dhttp/client.go
@@ -15,11 +15,8 @@
 package dhttp
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net"
 	"net/http"
 	"net/url"
@@ -27,16 +24,6 @@ import (
 
 	"github.com/exograd/go-log"
 )
-
-type RequestError struct {
-	Status   int
-	APIError *APIError
-	Message  string
-}
-
-func (err RequestError) Error() string {
-	return err.Message
-}
 
 type ClientCfg struct {
 	Log *log.Logger `json:"-"`
@@ -97,65 +84,5 @@ func (c *Client) SendRequest(method string, uri *url.URL, header map[string]stri
 		req.Header.Set(name, value)
 	}
 
-	res, err := c.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	if !(res.StatusCode >= 200 && res.StatusCode < 300) {
-		reqErr := &RequestError{
-			Status:   res.StatusCode,
-			APIError: nil,
-			Message: fmt.Sprintf("request failed with status %d",
-				res.StatusCode),
-		}
-
-		resBody, err := ioutil.ReadAll(res.Body)
-		if err == nil {
-			if res.Header.Get("Content-Type") == "application/json" {
-				var apiErr APIError
-				if err := json.Unmarshal(resBody, &apiErr); err == nil {
-					reqErr.APIError = &apiErr
-					reqErr.Message += ": " + apiErr.Message
-				} else {
-					c.Log.Error("cannot decode api error response: %v", err)
-				}
-			}
-
-			if reqErr.APIError == nil && len(resBody) > 0 {
-				reqErr.Message += ": " + string(resBody)
-			}
-		} else {
-			c.Log.Error("cannot read response body: %v", err)
-		}
-
-		return res, reqErr
-	}
-
-	return res, nil
-}
-
-func (c *Client) SendJSONRequest(method string, uri *url.URL, header map[string]string, value interface{}) (*http.Response, error) {
-	var body io.Reader
-
-	if value != nil {
-		var buf bytes.Buffer
-
-		encoder := json.NewEncoder(&buf)
-		if err := encoder.Encode(value); err != nil {
-			return nil, fmt.Errorf("cannot encode request body: %w", err)
-		}
-
-		body = &buf
-	}
-
-	if header == nil {
-		header = make(map[string]string)
-	}
-
-	if _, found := header["Content-Type"]; !found {
-		header["Content-Type"] = "application/json"
-	}
-
-	return c.SendRequest(method, uri, header, body)
+	return c.Do(req)
 }
